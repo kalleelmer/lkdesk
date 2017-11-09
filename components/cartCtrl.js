1f8cbe186e6704d8f695dff2f772d0c78d1c1964
Kristoffer <kristoffer.ipod@gmail.com>
2017-11-09 19:20:29 +0100

    Betalningsstöd

--- components/cartCtrl.js
+++ components/cartCtrl.js
@@ -3,20 +3,28 @@ var cartCtrl = function($filter, $scope, Core, $attrs, cartService, $location) {
   var $ctrl = this;
   $ctrl.cart = cartService.getCart();
 
-	$scope.selectedPerformance = {};
-	$scope.modaldata = {
-		categories: {},
-		rates: {}
-	};
-	$scope.prices = [];
-	$scope.show = {name: ""};
+  $scope.selectedPerformance = {};
+  $scope.modaldata = {
+    categories: {},
+    rates: {}
+  };
+  $scope.prices = [];
+  $scope.show = {
+    name: ""
+  };
 
   $scope.reload = function() {
     $location.path("/");
   }
 
   $scope.pay = function() {
-    $location.path("/");
+
+    Core.post("/" + $ctrl.cart.cartObject.id + "/payments", {method: "cash"}).then(function() {
+      $location.path("/");
+    }, function(error) {
+      console.log(error.status);
+    });
+
   }
 
   $scope.saveCustomer = function() {
@@ -26,59 +34,51 @@ var cartCtrl = function($filter, $scope, Core, $attrs, cartService, $location) {
 
   $scope.editShow = function(tickets) {
 
-		$scope.prices = [];
+    $scope.prices = [];
 
     var uniqueCategories = _.sortedUniqBy(tickets, function(o) {
       return o.category_id;
     });
 
-    Core.get("/admin/performances/" + tickets[0].performance_id).then(
-      function(response) {
+    Core.get("/admin/performances/" + tickets[0].performance_id).then(function(response) {
 
-				$scope.selectedPerformance = response.data;
+      $scope.selectedPerformance = response.data;
 
-				Core.get("/admin/shows/" + $scope.selectedPerformance.show_id + "/rates").then(
-		      function(response) {
-		        $scope.modaldata.rates = response.data;
-		      },
-		      function(response) {
-		        alert("Kunde inte hämta rates: " + response.status);
-		      });
-
-        Core.get("/admin/shows/" + $scope.selectedPerformance.show_id + "/categories")
-          .then(
-            function(response) {
-              $scope.modaldata.categories = response.data;
-              for (var a in $scope.modaldata.categories) {
-                Core.get("/desk/categories/" + $scope.modaldata.categories[a].id + "/prices").then(
-                  function(response) {
-
-                    $scope.prices = _.concat($scope.prices, response.data);
-
-										_.forEach(_.filter(tickets, {'category_id' : response.data[0].category_id}), function(ticket) {
-
-											_.find($scope.prices, {'rate_id': ticket.rate_id, 'category_id' : ticket.category_id}).count = _.filter(tickets, {'rate_id': ticket.rate_id, 'category_id' : ticket.category_id}).length;
-
-										});
-
-                  },
-                  function(error) {
-
-                  }
-                );
-              }
-
-            },
-            function(response) {
-              alert("Kunde inte hämta kategorier: " + response.status);
-            });
-
-      },
-      function(response) {
-        alert("Kunde inte hämta föreställningen: " + response.status);
+      Core.get("/admin/shows/" + $scope.selectedPerformance.show_id + "/rates").then(function(response) {
+        $scope.modaldata.rates = response.data;
+      }, function(response) {
+        alert("Kunde inte hämta rates: " + response.status);
       });
 
+      Core.get("/admin/shows/" + $scope.selectedPerformance.show_id + "/categories").then(function(response) {
+        $scope.modaldata.categories = response.data;
+        for (var a in $scope.modaldata.categories) {
+          Core.get("/desk/categories/" + $scope.modaldata.categories[a].id + "/prices").then(function(response) {
 
+            $scope.prices = _.concat($scope.prices, response.data);
+
+            _.forEach(_.filter(tickets, {'category_id': response.data[0].category_id}), function(ticket) {
+
+              _.find($scope.prices, {
+                'rate_id': ticket.rate_id,
+                'category_id': ticket.category_id
+              }).count = _.filter(tickets, {
+                'rate_id': ticket.rate_id,
+                'category_id': ticket.category_id
+              }).length;
+
+            });
+
+          }, function(error) {});
+        }
+
+      }, function(response) {
+        alert("Kunde inte hämta kategorier: " + response.status);
+      });
+
+    }, function(response) {
+      alert("Kunde inte hämta föreställningen: " + response.status);
+    });
 
   }
 
