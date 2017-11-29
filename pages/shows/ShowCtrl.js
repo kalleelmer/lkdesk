517dec80a9fac7f32cbb8b5ee853f79e94e5f312
Bluefade <ha.johan@gmail.com>
2017-11-29 11:33:15 +0100

    Clean up av controllers och ändringar i CSS

--- pages/shows/ShowCtrl.js
+++ pages/shows/ShowCtrl.js
@@ -2,8 +2,6 @@ var module = angular.module("lkticket.admin");
 
 var ShowCtrl = function($filter, $scope, $http, User, $routeParams, Core) {
 	var ctrl = this;
-
-
 };
 
 module.controller("ShowCtrl", ShowCtrl);
--- pages/shows/ShowsCtrl.js
+++ pages/shows/ShowsCtrl.js
@@ -8,29 +8,6 @@ var ShowsCtrl = function($scope, $http, User, Core) {
 		alert("Kunde inte hämta nöjan: " + response.status);
 	});
 
-	$scope.toggleEditMode = function(show) {
-		show.editToggle = !show.editToggle;
-	}
-
-	$scope.editNameOfShow = function(show) {
-		// TODO Lösa riktigt mot databasen
-		$scope.toggleEditMode(show);
-	}
-
-	$scope.deleteShow = function(show) {
-		// TODO Lösa riktigt mot databasen
-		$scope.shows.splice($scope.shows.indexOf(show), 1);
-	}
-
-	$scope.addNewShow = function(show) {
-		Core.post("/admin/shows", show).then(function(response){
-			$scope.shows.push(response.data);
-			show.name = "";
-		}, function(response) {
-			alert("kunde inte lägga till nöje: ", response.status);
-		});
-	}
-
 }
 
 module.controller("ShowsCtrl", ShowsCtrl);
