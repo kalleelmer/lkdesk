1b50b4fb6c439f086a444d70e4384dbe561f7b9f
Bluefade <ha.johan@gmail.com>
2017-12-16 16:00:36 +0100

    Rensning av gammal struktur

--- pages/shows/ShowCtrl.js
+++
@@ -1,7 +0,0 @@
-var module = angular.module("lkticket.admin");
-
-var ShowCtrl = function($filter, $scope, $http, User, $routeParams, Core) {
-	var ctrl = this;
-};
-
-module.controller("ShowCtrl", ShowCtrl);
