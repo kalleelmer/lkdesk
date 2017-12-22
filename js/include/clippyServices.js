36f684ef0d02ae7e6005c7981d9d757e160349ea
Kristoffer <elsanordstrom@Elsas-MacBook-Air-2.local>
2017-12-22 13:55:04 +0100

    clippy fix

--- js/include/clippyServices.js
+++ js/include/clippyServices.js
@@ -2,8 +2,4 @@ var module = angular.module("lkticket.admin");
 
 module.factory('clippyService', function(Core, $routeParams, $location) {
 
-  return {
-    say: function(swhatToSay) {
-
-    }
 });
