98ceb260581c9b8a1860462faaf9d45a01b2ea52
Kristoffer <elsanordstrom@Elsas-MacBook-Air-2.local>
2017-12-23 11:04:54 +0100

    Fix

--- pages/customers/customersCtrl.js
+++ pages/customers/customersCtrl.js
@@ -1,49 +1,5 @@
 var module = angular.module("lkticket.admin");
 
-<<<<<<< HEAD
-var CustomersCtrl = function($filter, cartService, $scope, $http, User,
-  $routeParams, Core, $sce, $location) {
-
-  Core.get("/desk/customers").then(function(response) {
-    $scope.customers = response.data;
-  }, function(response) {
-    alert("Kunde inte hämta kunder: " + response.status);
-  });
-
-  $scope.getCustomer = function(id) {
-    Core.get("/desk/customers/" + id).then(
-      function(response) {
-
-				console.log(response.data);
-        $scope.selectedCustomer = response.data;
-        Core.get("/desk/customers/" + id + "/orders/").then(
-          function(response) {
-            $scope.selectedCustomer.orders = response.data;
-          },
-          function(response) {
-            alert("Kunde inte hämta kunder: " + response.status);
-          });
-      },
-      function(response) {
-        alert("Kunde inte hämta kund: " + response.status);
-      });
-  }
-
-  $scope.selectOrder = function(order) {
-    cartService.getCartById(order);
-  }
-
-  $scope.createCustomer = function(customer) {
-    Core.post("/desk/customers/", customer).then(
-      function(response) {
-				$scope.customers.push(response.data);
-      },
-      function(error) {
-
-      }
-    )
-  }
-=======
 var CustomersCtrl = function($filter, Cart, $scope, $http, User,
 	$routeParams, Core, $sce, $location) {
 
@@ -68,10 +24,19 @@ var CustomersCtrl = function($filter, Cart, $scope, $http, User,
 			});
 	}
 
+	$scope.createCustomer = function(customer) {
+    Core.post("/desk/customers/", customer).then(
+      function(response) {
+				$scope.customers.push(response.data);
+      },
+      function(error) {
+
+      }
+    )};
+
 	$scope.selectOrder = function(order) {
 		Cart.getCartById(order);
 	}
->>>>>>> 17da9c0efd21a49b18b1be40b0e19f34110f5fa8
 
 }
 
