6110314d3ab2548a5bbc5ed605dbb1016612d987
Kristoffer <elsanordstrom@Elsas-MacBook-Air-2.local>
2017-12-23 11:02:20 +0100

    Possible to create customers

--- pages/customers/customersCtrl.js
+++ pages/customers/customersCtrl.js
@@ -1,32 +1,47 @@
 var module = angular.module("lkticket.admin");
 
 var CustomersCtrl = function($filter, cartService, $scope, $http, User,
-	$routeParams, Core, $sce, $location) {
+  $routeParams, Core, $sce, $location) {
 
-	Core.get("/desk/customers").then(function(response) {
-		$scope.customers = response.data;
-	}, function(response) {
-		alert("Kunde inte hämta kunder: " + response.status);
-	});
+  Core.get("/desk/customers").then(function(response) {
+    $scope.customers = response.data;
+  }, function(response) {
+    alert("Kunde inte hämta kunder: " + response.status);
+  });
 
-	$scope.getCustomer = function(id) {
-		Core.get("/desk/customers/" + id).then(
-			function(response) {
-				$scope.selectedCustomer = response.data;
-				Core.get("/desk/customers/" + id + "/orders/").then(
-					function(response) {
-						$scope.selectedCustomer.orders = response.data;
-					}, function(response) {
-						alert("Kunde inte hämta kunder: " + response.status);
-					});
-			}, function(response) {
-				alert("Kunde inte hämta kund: " + response.status);
-			});
-	}
+  $scope.getCustomer = function(id) {
+    Core.get("/desk/customers/" + id).then(
+      function(response) {
 
-	$scope.selectOrder = function(order) {
-		cartService.getCartById(order);
-	}
+				console.log(response.data);
+        $scope.selectedCustomer = response.data;
+        Core.get("/desk/customers/" + id + "/orders/").then(
+          function(response) {
+            $scope.selectedCustomer.orders = response.data;
+          },
+          function(response) {
+            alert("Kunde inte hämta kunder: " + response.status);
+          });
+      },
+      function(response) {
+        alert("Kunde inte hämta kund: " + response.status);
+      });
+  }
+
+  $scope.selectOrder = function(order) {
+    cartService.getCartById(order);
+  }
+
+  $scope.createCustomer = function(customer) {
+    Core.post("/desk/customers/", customer).then(
+      function(response) {
+				$scope.customers.push(response.data);
+      },
+      function(error) {
+
+      }
+    )
+  }
 
 }
 
