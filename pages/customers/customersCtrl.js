a4b25dc7490f734156db629e63f1471ae10e7c23
kristoffer <kristoffer.nordstrom@outlook.com>
2018-01-03 20:30:25 +0100

    Fungerar att koppla en bokning till en kund

--- pages/customers/customersCtrl.js
+++ pages/customers/customersCtrl.js
@@ -1,42 +1,53 @@
 var module = angular.module("lkticket.admin");
 
 var CustomersCtrl = function($filter, Cart, $scope, $http, User,
-	$routeParams, Core, $sce, $location, Clippy) {
+  $routeParams, Core, $sce, $location, Clippy) {
 
-	Core.get("/desk/customers").then(function(response) {
-		$scope.customers = response.data;
-	}, function(response) {
-		alert("Kunde inte hämta kunder: " + response.status);
-	});
+  $scope.cart = Cart.getCart();
 
-	$scope.getCustomer = function(id) {
-		Core.get("/desk/customers/" + id).then(
-			function(response) {
-				$scope.selectedCustomer = response.data;
-				Core.get("/desk/customers/" + id + "/orders/").then(
-					function(response) {
-						$scope.selectedCustomer.orders = response.data;
-					}, function(response) {
-						alert("Kunde inte hämta kunder: " + response.status);
-					});
-			}, function(response) {
-				alert("Kunde inte hämta kund: " + response.status);
-			});
-	}
+  Core.get("/desk/customers").then(function(response) {
+    $scope.customers = response.data;
+  }, function(response) {
+    alert("Kunde inte hämta kunder: " + response.status);
+  });
 
-	$scope.createCustomer = function(customer) {
+  $scope.getCustomer = function(id) {
+    Core.get("/desk/customers/" + id).then(
+      function(response) {
+        $scope.selectedCustomer = response.data;
+        Core.get("/desk/customers/" + id + "/orders/").then(
+          function(response) {
+            $scope.selectedCustomer.orders = response.data;
+          },
+          function(response) {
+            alert("Kunde inte hämta kunder: " + response.status);
+          });
+      },
+      function(response) {
+        alert("Kunde inte hämta kund: " + response.status);
+      });
+  }
+
+  $scope.createCustomer = function(customer) {
     Core.post("/desk/customers/", customer).then(
       function(response) {
-				$scope.customers.push(response.data);
+        $scope.customers.push(response.data);
       },
       function(error) {
-				Clippy.say("Fel i skapning av kund: " + error.status)
+        Clippy.say("Fel i skapning av kund: " + error.status)
       }
-    )};
+    )
+  };
 
-	$scope.selectOrder = function(order) {
-		Cart.getCartById(order);
-	}
+  $scope.selectOrder = function(order) {
+    Cart.getCartById(order);
+  }
+
+  $scope.assignCartToCustomer = function() {
+    Cart.assignCartToCustomer($scope.selectedCustomer.id, function() {
+    	$scope.getCustomer($scope.selectedCustomer.id);
+    });
+  }
 
 }
 
