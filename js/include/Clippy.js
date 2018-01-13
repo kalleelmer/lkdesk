c320a80ce2665449f136c45055836d56da19a588
kristoffer <kristoffer.nordstrom@outlook.com>
2018-01-13 11:48:43 +0100

    Clippy lyssnar efter websocket

--- js/include/Clippy.js
+++ js/include/Clippy.js
@@ -4,33 +4,48 @@ module.factory('Clippy', function() {
 
 	var clip = 0;
 
-	var load = function () {
-		clippy.load(localStorage.mascot || 'Clippy', function(agent) {
-			// Do anything with the loaded agent
-			agent.show();
-			agent.speak("IMAGINAL!!")
+  var connection = new WebSocket('ws://localhost:8080', ['soap', 'xmpp']);
 
-			clip = agent;
+  connection.onopen = function() {
+  };
 
-			clip.speak("HEJ!");
+  // Log errors
+  connection.onerror = function(error) {
+    console.log('WebSocket Error ' + error);
+  };
 
-		});
-	}
-
-	load();
-
-	return {
-		setMascot : function functionName(n) {
-			localStorage.mascot = n;
-			clip.hide();
-			load();
-		},
-		say : function(say) {
-			console.log(clip);
-			clip.speak(say);
-		},
-		play : function(animation) {
-			clip.play(animation);
+  // Log messages from the server
+  connection.onmessage = function(e) {
+		if (clip != 0) {
+			clip.speak(e.data);
 		}
-	}
+  };
+
+  var load = function() {
+    clippy.load(localStorage.mascot || 'Clippy', function(agent) {
+      // Do anything with the loaded agent
+      agent.show();
+      agent.speak("IMAGINAL!!")
+
+      clip = agent;
+
+    });
+  }
+
+  load();
+
+  return {
+    setMascot: function functionName(n) {
+      localStorage.mascot = n;
+      clip.hide();
+      load();
+    },
+    say: function(say) {
+      console.log(clip);
+      clip.speak(say);
+    },
+    play: function(animation) {
+      clip.play(animation);
+    }
+  }
 });
