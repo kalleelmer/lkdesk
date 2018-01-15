8d468eb9a0d11e3a151695017c93b735d45288c7
Kalle Elmér <kalle@silvertejp.nu>
2018-01-15 20:42:09 +0100

    Revert "Clippy lyssnar efter websocket"
    This reverts commit c320a80ce2665449f136c45055836d56da19a588.

--- js/include/Clippy.js
+++ js/include/Clippy.js
@@ -4,48 +4,33 @@ module.factory('Clippy', function() {
 
 	var clip = 0;
 
-  var connection = new WebSocket('ws://localhost:8080', ['soap', 'xmpp']);
+	var load = function () {
+		clippy.load(localStorage.mascot || 'Clippy', function(agent) {
+			// Do anything with the loaded agent
+			agent.show();
+			agent.speak("IMAGINAL!!")
 
-  connection.onopen = function() {
-  };
+			clip = agent;
 
-  // Log errors
-  connection.onerror = function(error) {
-    console.log('WebSocket Error ' + error);
-  };
+			clip.speak("HEJ!");
 
-  // Log messages from the server
-  connection.onmessage = function(e) {
-		if (clip != 0) {
-			clip.speak(e.data);
+		});
+	}
+
+	load();
+
+	return {
+		setMascot : function functionName(n) {
+			localStorage.mascot = n;
+			clip.hide();
+			load();
+		},
+		say : function(say) {
+			console.log(clip);
+			clip.speak(say);
+		},
+		play : function(animation) {
+			clip.play(animation);
 		}
-  };
-
-  var load = function() {
-    clippy.load(localStorage.mascot || 'Clippy', function(agent) {
-      // Do anything with the loaded agent
-      agent.show();
-      agent.speak("IMAGINAL!!")
-
-      clip = agent;
-
-    });
-  }
-
-  load();
-
-  return {
-    setMascot: function functionName(n) {
-      localStorage.mascot = n;
-      clip.hide();
-      load();
-    },
-    say: function(say) {
-      console.log(clip);
-      clip.speak(say);
-    },
-    play: function(animation) {
-      clip.play(animation);
-    }
-  }
+	}
 });
