2929725c49c25974d6be5062189b681b9f657751
Kalle Elmér <kalle@silvertejp.nu>
2018-02-22 18:27:37 +0100

    Inkludera föreställnings pålägg i priser.

--- components/addTicketsModalCtrl.js
+++ components/addTicketsModalCtrl.js
@@ -1,80 +1,72 @@
 var module = angular.module("lkticket.admin");
-var addTicketsModalCtrl = function($filter, $scope, Core, Cart, $location, $attrs) {
-  var $ctrl = this
-  $scope.uid = $attrs.uid;
+var addTicketsModalCtrl = function($filter, $scope, Core, Cart, $location,
+	$attrs) {
+	var $ctrl = this
+	$scope.uid = $attrs.uid;
 
+	$ctrl.$onChanges = function(change) {
 
-  $ctrl.$onChanges = function(change) {
+		$scope.prices = angular.copy($ctrl.prices);
 
-    $scope.prices = angular.copy($ctrl.prices);
+	}
 
-  }
+	$scope.ticketAdded = function() {
 
-  $scope.ticketAdded = function(rid, cid, n) {
+	}
 
-		price = $scope.getPrice(rid, cid);
+	$scope.getNumberOfTickets = function() {
+		return _.sumBy($scope.prices, function(o) {
+			return o.count;
+		});
+	}
 
-		if (!price.count) {
-			price.count = 0;
+	$scope.getAmount = function() {
+
+		var tickets = _.filter($scope.prices, function(o) {
+			return o.count > 0
+		});
+		return _.sumBy(tickets, function(o) {
+			return (o.count * o.price);
+		});
+	}
+
+	$scope.getPrice = function(rate_id, category_id) {
+
+		var price = _.find($scope.prices, {
+			'rate_id' : rate_id,
+			'category_id' : category_id
+		});
+
+		if (_.isNil(price)) {
+			return null;
+		} else {
+			return price;
 		}
+	}
 
-		if (price.count == 0 && n == -1) {
-			return;
-		}
+	$scope.calculatePrice = function(price) {
+		return Math.max(0, price + $ctrl.selectedperformance.surcharge);
+	}
 
-		price.count += n;
+	$scope.addTicketsToCart = function() {
+		var tickets = _.filter($scope.prices, function(o) {
+			return o.count > 0
+		});
 
-  }
+		_.forEach(tickets, function(ticket) {
+			ticket.performance = $ctrl.selectedperformance;
+			Cart.addTicket(ticket, function(response) {
+				if (response == true) {
 
-  $scope.getNumberOfTickets = function() {
-    return _.sumBy($scope.prices, function(o) {
-      return o.count;
-    });
-  }
+				} else {
+					alert("Biljetterna är slut...");
+				}
+			});
+		});
 
-  $scope.getAmount = function() {
+		$scope.prices = angular.copy($ctrl.prices);
 
-    var tickets = _.filter($scope.prices, function(o) {
-      return o.count > 0
-    });
-    return _.sumBy(tickets, function(o) {
-      return (o.count * o.price);
-    });
-  }
-
-  $scope.getPrice = function(rate_id, category_id) {
-
-    var price = _.find($scope.prices, {
-      'rate_id': rate_id,
-      'category_id': category_id
-    });
-
-    if (_.isNil(price)) {
-      return null;
-    } else {
-      return price;
-    }
-  }
-
-  $scope.addTicketsToCart = function() {
-    var tickets = _.filter($scope.prices, function(o) {
-      return o.count > 0
-    });
-
-    _.forEach(tickets, function(ticket) {
-      ticket.performance = $ctrl.selectedperformance;
-      Cart.addTicket(ticket, function(response) {
-        if (response == true) {
-
-        } else {
-          alert("Biljetterna är slut...");
-        }
-      });
-    });
-
-    $scope.prices = angular.copy($ctrl.prices);
-
-  }
+	}
 
 }
 
